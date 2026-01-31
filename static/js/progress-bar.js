53b31b40ed458c9d6af946bcc19865a1c30e56dd
Adarsh <adarshverma0524@gmail.com>
2026-01-31 23:12:51 +0530

    deleted unnecessary files:

--- static/js/progress-bar.js
+++
@@ -1,7 +0,0 @@
-// progress-bar.js
-window.addEventListener('scroll', () => {
-  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;
-  const pct = (scrolled / docHeight) * 100;
-  document.getElementById('scroll-progress').style.width = pct + '%';
-});
